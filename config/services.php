7cdba96adae71203b55bb70fcc80d509b81f1aad
matheo <mdaninos@apple.com>
2023-10-23 14:24:25 +0200

    Add Listener to PreAssetsCompileEventListener

--- config/services.php
+++ config/services.php
@@ -5,6 +5,8 @@ use Symfonycasts\SassBundle\Command\SassBuildCommand;
 use Symfonycasts\SassBundle\SassBuilder;
 use Symfonycasts\SassBundle\AssetMapper\SassCssCompiler;
 use Symfonycasts\SassBundle\AssetMapper\SassPublicPathAssetPathResolver;
+use Symfonycasts\SassBundle\Listener\PreAssetsCompileEventListener;
+use Symfony\Component\AssetMapper\Event\PreAssetsCompileEvent;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\abstract_arg;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
@@ -35,10 +37,18 @@ return static function (ContainerConfigurator $container) {
                 abstract_arg('path to css output directory'),
                 service('sass.builder'),
             ])
+
         ->set('sass.public_asset_path_resolver', SassPublicPathAssetPathResolver::class)
         ->decorate('asset_mapper.public_assets_path_resolver')
         ->args([
             service('.inner')
         ])
+
+        ->set('sass.listener.pre_assets_compile', PreAssetsCompileEventListener::class)
+        ->tag('kernel.event_listener', [
+            'event' => PreAssetsCompileEvent::class,
+            'method' => '__invoke'
+        ])
+    ;
     ;
 };
---
+++ src/Listener/PreAssetsCompileEventListener.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the SymfonyCasts SassBundle package.
+ * Copyright (c) SymfonyCasts <https://symfonycasts.com/>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfonycasts\SassBundle\Listener;
+
+use Symfony\Component\AssetMapper\Event\PreAssetsCompileEvent;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfonycasts\SassBundle\SassBuilder;
+
+class PreAssetsCompileEventListener
+{
+    public function __construct(private readonly SassBuilder $sassBuilder) {}
+
+    public function __invoke(PreAssetsCompileEvent $preAssetsCompileEvent): void
+    {
+        $this->sassBuilder->setOutput(
+            new SymfonyStyle(
+                new ArrayInput([]),
+                $preAssetsCompileEvent->getOutput()
+            )
+        );
+
+        $process = $this->sassBuilder->runBuild(false);
+
+        if ($process->isSuccessful()) {
+            return;
+        }
+
+        throw new \RuntimeException($process->getErrorOutput());
+    }
+}
