04f0769afb0b0c0e48ff0927d9d9f3f9fde572e0
Evert Harmeling <evertharmeling@gmail.com>
2023-11-16 11:07:14 +0100

    Added SassCssCompilerTest

---
+++ tests/AssetMapper/SassCssCompilerTest.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfonycasts\SassBundle\Tests\AssetMapper;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\AssetMapper\AssetMapperInterface;
+use Symfony\Component\AssetMapper\MappedAsset;
+use Symfonycasts\SassBundle\AssetMapper\SassCssCompiler;
+use Symfonycasts\SassBundle\SassBuilder;
+
+use function file_put_contents;
+
+final class SassCssCompilerTest extends TestCase
+{
+    private const CSS_DIR = __DIR__.'/../fixtures/var/sass';
+
+    public function setUp(): void
+    {
+        if (!is_dir(self::CSS_DIR)) {
+            mkdir(self::CSS_DIR);
+        }
+    }
+
+    public function testCompileSingleSassPath(): void
+    {
+        $scssFile = __DIR__.'/../fixtures/assets/app.scss';
+        $scssPaths = [
+            $scssFile
+        ];
+        $cssFile = self::CSS_DIR.'/app.output.css';
+
+        $compiler = new SassCssCompiler(
+            $scssPaths,
+            self::CSS_DIR,
+            $this->createSassBuilder($scssPaths, self::CSS_DIR)
+        );
+
+        $mappedAsset = new MappedAsset(
+            'app.scss',
+            $scssFile,
+            'app.css'
+        );
+
+        file_put_contents($cssFile, <<<EOF
+            p {
+               color: red;
+            }
+            EOF
+        );
+
+        $compiledContent = $compiler->compile(
+            file_get_contents($scssFile),
+            $mappedAsset,
+            $this->createMock(AssetMapperInterface::class)
+        );
+
+        $this->assertStringEqualsFile(
+            $cssFile,
+            $compiledContent
+        );
+    }
+
+    public function testCompileNamedSassPath()
+    {
+        $scssFile = __DIR__.'/../fixtures/assets/admin/app.scss';
+        $scssPaths = [
+            'admin' => $scssFile
+        ];
+        $cssFile = self::CSS_DIR.'/admin.output.css';
+
+        $compiler = new SassCssCompiler(
+            $scssPaths,
+            self::CSS_DIR,
+            $this->createSassBuilder($scssPaths, self::CSS_DIR)
+        );
+
+        $mappedAsset = new MappedAsset(
+            'admin/app.scss',
+            $scssFile,
+            'admin.css'
+        );
+
+        file_put_contents($cssFile, <<<EOF
+            p {
+               color: blue;
+            }
+            EOF
+        );
+
+        $compiledContent = $compiler->compile(
+            file_get_contents($scssFile),
+            $mappedAsset,
+            $this->createMock(AssetMapperInterface::class)
+        );
+
+        $this->assertStringEqualsFile(
+            $cssFile,
+            $compiledContent
+        );
+    }
+
+    private function createSassBuilder(array $sassPaths, string $cssPath): SassBuilder
+    {
+        return new SassBuilder(
+            $sassPaths,
+            $cssPath,
+            __DIR__.'/../fixtures',
+            null,
+            false
+        );
+    }
+}
